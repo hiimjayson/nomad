cf104b5cf2b97a3085a389b4ac54e069935012b5
hiimjayson <hiimjaysondev@gmail.com>
2024-12-08 22:58:20 +0900

    feat(backend): supabase 클라이언트 추가

--- apps/backend/env.d.ts
+++ apps/backend/env.d.ts
@@ -7,5 +7,7 @@ declare namespace NodeJS {
     NCP_CALLING_NUMBER: string;
     UPSTASH_REDIS_REST_URL: string;
     UPSTASH_REDIS_REST_TOKEN: string;
+    SUPABASE_URL: string;
+    SUPABASE_ANON_KEY: string;
   }
 }
