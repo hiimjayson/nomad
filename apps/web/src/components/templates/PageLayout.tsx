4a9b860d16d0c8a19eb27347eef9e1f6cc2c1e03
hiimjayson <hiimjaysondev@gmail.com>
2024-12-19 00:11:34 +0900

    fix: PageLayout.Header 위치 문제 수정
    - fixed라서 layout content box 내부에 들어가지 않는 오류

--- apps/web/src/components/templates/PageLayout.tsx
+++ apps/web/src/components/templates/PageLayout.tsx
@@ -16,7 +16,7 @@ export function PageLayout({ children }: { children: React.ReactNode }) {
 PageLayout.Header = function Header() {
   return (
     <>
-      <header className="flex justify-between items-center p-4 fixed w-full max-w-screen-md  h-16 border-b-2 border-typo-time bg-background-default">
+      <header className="flex justify-between items-center p-4 fixed ml-[-2px] w-full max-w-screen-md md:border-x-2 h-16 border-b-2 border-typo-time bg-background-default">
         <Link href="/">
           <Image src="/images/logo.svg" alt="로고" width={120} height={22} />
         </Link>
