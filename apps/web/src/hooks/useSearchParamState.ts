c36bdabc972763cf291102f68bc4087d57232b1f
hiimjayson <hiimjaysondev@gmail.com>
2024-12-18 23:41:26 +0900

    feat(web): useSearchParamState 훅에 로컬 상태 추가

--- apps/web/src/hooks/useSearchParamState.ts
+++ apps/web/src/hooks/useSearchParamState.ts
@@ -1,5 +1,5 @@
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { isEmptyStringOrNil } from "@/lib/string";
 
 export function useSearchParamState(
@@ -7,26 +7,31 @@ export function useSearchParamState(
   strategy: "replace" | "push" = "replace"
 ) {
   const searchParams = useSearchParams();
+  const [_value, _setValue] = useState<string | null>(null);
+
+  useEffect(() => {
+    _setValue(searchParams.get(key));
+  }, [searchParams, key]);
 
   const pathname = usePathname();
   const router = useRouter();
 
   return useMemo(() => {
-    const value = searchParams.get(key);
-
     return [
-      value,
+      _value,
       function set(value: string | null) {
         const newSearchParams = new URLSearchParams(searchParams);
 
         if (isEmptyStringOrNil(value)) {
           newSearchParams.delete(key);
+          _setValue(null);
         } else {
           newSearchParams.set(key, value);
+          _setValue(value);
         }
 
         router[strategy](`${pathname}?${newSearchParams.toString()}`);
       },
     ] as const;
-  }, [searchParams, key, router, strategy, pathname]);
+  }, [searchParams, key, router, strategy, pathname, _value]);
 }
